5b1d51c00a1071c6c235271ac2d07ae5362a444a
Maciej Wiatr <maciekwiatr17@gmail.com>
2020-05-30 10:37:33 +0200

    Add parcel, finish section 2

--- adopt-me/src/app.js
+++ adopt-me/src/app.js
@@ -1,11 +1,6 @@
-// Your code is going to go here
-const Pet = ({ name, animal, breed }) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, name),
-        React.createElement("h2", {}, animal),
-        React.createElement("h2", {}, breed),
-    ]);
-};
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
     return React.createElement("div", {}, [
@@ -27,4 +22,4 @@ const App = () => {
         }),
     ]);
 };
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
