79b72a5478555d9fbc0b089a360fac6e57c610e6
Maciej Wiatr <maciekwiatr17@gmail.com>
2020-05-30 14:49:27 +0200

    Finish effects section

--- adopt-me/src/SearchParams.jsx
+++ adopt-me/src/SearchParams.jsx
@@ -1,18 +1,25 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
-import useDropdown from "./useDropdown";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown.jsx";
 
 const SearchParams = () => {
     const [location, setLocation] = useState("Seattle,WA");
-    // const [animal, setAnimal] = useState("dog");
-    // const [breed, setBreed] = useState("");
     const [breeds, setBreeds] = useState([]);
-    const [animal, AnimalDropdown] = useDropdown("Animal", "Dog", ANIMALS);
-    const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+    const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+    const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+
+    useEffect(() => {
+        setBreeds([]);
+        setBreed("");
+
+        pet.breeds(animal).then(({ breeds: apiBreeds }) => {
+            const breedStrings = apiBreeds.map(({ name }) => name);
+            setBreeds(breedStrings);
+        }, console.error);
+    }, [animal, setBreeds, setBreed]);
 
     return (
         <div className="search-params">
-            <h1>{location}</h1>
             <form>
                 <label htmlFor="location">
                     Location
--- adopt-me/src/app.js
+++ adopt-me/src/app.js
@@ -4,10 +4,12 @@ import SearchParams from "./SearchParams.jsx";
 
 const App = () => {
     return (
-        <div>
-            <h1 id="something-important">Adopt me!</h1>
-            <SearchParams />
-        </div>
+        <React.StrictMode>
+            <div>
+                <h1 id="something-important">Adopt me!</h1>
+                <SearchParams />
+            </div>
+        </React.StrictMode>
     );
 };
 render(<App />, document.getElementById("root"));
