06f90cb9fd60fa85244e8d98e024dbf57308a142
Dettlaff <maciekwiatr17@gmail.com>
2020-06-06 10:01:57 +0200

    Fix api calls

--- adopt-me/src/Details.jsx
+++ adopt-me/src/Details.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
+import { navigate } from "@reach/router";
+import Modal from "./Modal.jsx";
 import Carousel from "./Carousel.jsx";
 import ErrorBoundary from "./ErrorBoundary.jsx";
+import ThemeContext from "./ThemeContext.jsx";
+
 class Details extends React.Component {
-    state = { loading: true, name: "" };
+    state = { loading: true, showModal: false };
 
     componentDidMount() {
         pet.animal(this.props.id).then(({ animal }) => {
             this.setState({
+                url: animal.url,
                 name: animal.name,
                 animal: animal.type,
                 location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
@@ -19,6 +24,8 @@ class Details extends React.Component {
         }, console.error);
     }
 
+    toggleModal = () => this.setState({ showModal: !this.state.showModal });
+    adopt = () => navigate(this.state.url);
     render() {
         if (this.state.loading) {
             return <h1>Loading ... </h1>;
@@ -30,6 +37,7 @@ class Details extends React.Component {
             description,
             name,
             media,
+            showModal,
         } = this.state;
 
         return (
@@ -37,9 +45,33 @@ class Details extends React.Component {
                 <Carousel media={media}></Carousel>
                 <div>
                     <h1>{name}</h1>
-                    <h2>{`${animal} - ${breed} - ${location}`}</h2>
-                    <button>Adopt {name}</button>
+                    <h2>{`${animal} - ${breed}  - ${location}`}</h2>
+                    <ThemeContext.Consumer>
+                        {([theme]) => (
+                            <button
+                                onClick={this.toggleModal}
+                                style={{ backgroundColor: theme }}
+                            >
+                                Adopt {name}
+                            </button>
+                        )}
+                    </ThemeContext.Consumer>
+
                     <p>{description}</p>
+
+                    {showModal ? (
+                        <Modal>
+                            <div>
+                                <h1>Would you like to adopt {name}?</h1>
+                                <div className="buttons">
+                                    <button onClick={this.adopt}>Yes</button>
+                                    <button onClick={this.toggleModal}>
+                                        No, I am a monster
+                                    </button>
+                                </div>
+                            </div>
+                        </Modal>
+                    ) : null}
                 </div>
             </div>
         );
--- adopt-me/src/SearchParams.jsx
+++ adopt-me/src/SearchParams.jsx
@@ -1,14 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown.jsx";
 import Results from "./Results.jsx";
+import ThemeContext from "./ThemeContext.jsx";
 
 const SearchParams = () => {
-    const [location, setLocation] = useState("Seattle,WA");
+    const [location, setLocation] = useState("Seattle, WA");
     const [breeds, setBreeds] = useState([]);
     const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
     const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
     const [pets, setPets] = useState([]);
+    const [theme, setTheme] = useContext(ThemeContext);
 
     async function requestPets() {
         const { animals } = await pet.animals({
@@ -49,7 +51,20 @@ const SearchParams = () => {
                 </label>
                 <AnimalDropdown />
                 <BreedDropdown />
-                <button>Submit</button>
+                <label htmlFor="theme">
+                    Theme
+                    <select
+                        value={theme}
+                        onChange={(e) => setTheme(e.target.value)}
+                        onBlur={(e) => setTheme(e.target.value)}
+                    >
+                        <option value="peru">Peru</option>
+                        <option value="darkblue">Darkblue</option>
+                        <option value="mediumorchid">Mediumorchid</option>
+                        <option value="chartreuse">Chartreuse</option>
+                    </select>
+                </label>
+                <button style={{ backgroundColor: theme }}>Submit</button>
             </form>
             <Results pets={pets}></Results>
         </div>
